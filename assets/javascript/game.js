b86271dc6f5918002ea36c283c79a9657db85323
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-03-08 03:51:17 -0600

    added animate and still functionality to gifs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // 1. global variables 
 var topics = ["Happy", "Annoyed", "Excited", "Amused", "Content", "Cranky", "Stressed", "Relaxed", "Nervous", "Defeated", "Uncertain", "Hopeful", "Impatient", "Paranoid", "Awesome", "Accomplished"];
-
+gif = "";
 // 2. create buttons in HTML by using a loop that appends a button for each string in the array
 function renderButtons() {
     $("#moods-view").empty();
@@ -36,15 +36,36 @@ $("button").on("click", function() {
             for (var i = 0; i < results.length; i++) {
                 var gifDiv = $("<div>");
                 var p = $("<p>").text("Rating: " +results[i].rating);
-                console.log("Rating: " + results[i].rating);
+                //add attributes to gifs on page to set to animate or still 
                 var topicImage = $("<img>");
-                topicImage.attr("src", results[i].images.fixed_height.url);
-                gifDiv.append(p);
                 gifDiv.append(topicImage);
+                gifDiv.append(p);
                 $("#gif-area").prepend(gifDiv);
-          }
-        });
+                var gifObj = response.data[i];
+                var gif = gifObj.images;
+                
+                //topicImage.attr("src", response.data[i].images.fixed_height.url);
+                    topicImage.attr({
+                        src: gif.fixed_height_still.url,
+                        "data-animate": gif.fixed_height.url,
+                        "data-still" : gif.fixed_height_still.url,
+                        "data-state": "still", 
+                        class: "gif",
+                    });
 
+                // gifs should change from still to animated when clicked, back to animated when clicked again. 
+                $(".gif").on("click", function() {
+                    var state = $(this).attr("data-state");
+                    if (state === "still") {
+                      $(this).attr("src", $(this).attr("data-animate"));
+                      $(this).attr("data-state", "animate");
+                    } else {
+                      $(this).attr("src", $(this).attr("data-still"));
+                      $(this).attr("data-state", "still");
+                    }
+                });
+        }
+    });
 
     });
 
