0d8c0bbc8f84a1854091750a0f2c91aa0138c954
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-03-08 02:27:28 -0600

    updated js so gifs print to page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // 1. global variables 
-var topics = ["happy", "sad", "angry", "sick", "excited", "amused", "calm", "content", "annoyed", "cranky", "stressed", "relaxed", "nervous", "defeated", "uncertain", "funny", "hopeful", "impatient", "paranoid", "awesome", "accomplished", "shitty", "pissed"];
+var topics = ["Happy", "Annoyed", "Excited", "Amused", "Content", "Cranky", "Stressed", "Relaxed", "Nervous", "Defeated", "Uncertain", "Hopeful", "Impatient", "Paranoid", "Awesome", "Accomplished"];
 
 // 2. create buttons in HTML by using a loop that appends a button for each string in the array
 function renderButtons() {
@@ -22,5 +22,32 @@ function renderButtons() {
     renderButtons();
     });
 
-    // Calling the renderButtons function at least once to display the initial list of movies
+    // Calling the renderButtons function at least once to display the initial buttons
     renderButtons();
+
+//3. When the user clicks on of the mood buttons grab 10 static gifs from giphy api and places on page
+$("button").on("click", function() {
+    var x = $(this).data("name");
+    console.log(x);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+x+"&api_key=on0VHNWeZ9CD3439RIQId67SGJH7LLh7&limit=10";
+    $.ajax({url: queryURL,method:"GET"})
+        .done(function(response){
+        var results = response.data;
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                var p = $("<p>").text("Rating: " +results[i].rating);
+                console.log("Rating: " + results[i].rating);
+                var topicImage = $("<img>");
+                topicImage.attr("src", results[i].images.fixed_height.url);
+                gifDiv.append(p);
+                gifDiv.append(topicImage);
+                $("#gif-area").prepend(gifDiv);
+          }
+        });
+
+
+    });
+
+
+
+
